781da2c882b4b5854a1d43926d20d42804b96be1
mariamny <mariamnydelvalle@gmail.com>
2025-07-30 17:37:42 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'home.html', {'name': 'Mariamny Ramirez'})
+from .models import Movie
 
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {
+    'searchTerm': searchTerm,
+    'movies': movies,
+    'name': 'Mariamny Ramírez'
+    })
+
+
+def about(request):
+    return render(request, 'about.html')
