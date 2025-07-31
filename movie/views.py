042de12aa6ca99c0f10966ae566190f8de481165
mariamny <mariamnydelvalle@gmail.com>
2025-07-30 22:39:47 -0500

    Mejora la búsqueda ignorando tildes con unidecode

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from unidecode import unidecode
 
 from .models import Movie
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
+        normalized_term = unidecode(searchTerm.lower())
+        all_movies = Movie.objects.all()
+        movies = [movie for movie in all_movies if normalized_term in unidecode(movie.title.lower())]
     else:
         movies = Movie.objects.all()
     return render(request, 'home.html', {
